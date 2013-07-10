b35127eb777c4445b74a6bcd9577c7aeb9820b41
Seth <sethdturner23@gmail.com>
2013-07-10 00:02:05 -0400

    initial commit

--- ElasticTranscoder.php
+++ ElasticTranscoder.php
@@ -132,7 +132,7 @@ class AWS_ET {
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Cancel a job
   *
@@ -145,7 +145,7 @@ class AWS_ET {
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Create a new pipeline
   *
@@ -155,7 +155,7 @@ class AWS_ET {
   * @param string $role IAM Amazon Resource Name (ARN)
   * @param array $notifications notification settings
   * @return array | false
-  */  
+  */
   public static function createPipeline($name, $inputBucket, $outputBucket, $role, $notifications = array()) {
     self::$HttpRequestMethod = 'POST';
     self::$Uri = '/2012-09-25/pipelines';
@@ -175,39 +175,39 @@ class AWS_ET {
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Get a list of pipelines associated with account
   *
   * @return array | false
-  */  
+  */
   public static function listPipelines() {
     self::$HttpRequestMethod = 'GET';
     self::$Uri = '/2012-09-25/pipelines';
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Get info about a pipeline
   *
   * @param string $pipelineId pipeline ID
   * @return array | false
-  */  
+  */
   public static function readPipeline($pipelineId) {
     self::$HttpRequestMethod = 'GET';
     self::$Uri = '/2012-09-25/pipelines/'.$pipelineId;
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Update settings for a pipeline
   *
   * @param string $pipelineId pipeline ID
   * @param array $updates updates
   * @return array | false
-  */    
+  */
   public static function updatePipeline($pipelineId, $updates) {
     self::$HttpRequestMethod = 'PUT';
     self::$Uri = '/2012-09-25/pipelines/'.$pipelineId;
@@ -216,14 +216,14 @@ class AWS_ET {
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Update pipeline status (active/paused)
   *
   * @param string $pipelineId pipeline ID
   * @param array $status new status
   * @return array | false
-  */   
+  */
   public static function updatePipelineStatus($pipelineId, $status) {
     self::$HttpRequestMethod = 'POST';
     self::$Uri = '/2012-09-25/pipelines/'.$pipelineId.'/status';
@@ -232,14 +232,14 @@ class AWS_ET {
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Update pipeline notification settings
   *
   * @param string $pipelineId pipeline ID
   * @param array $notifications new notification settings
   * @return array | false
-  */   
+  */
   public static function updatePiplineNotifications($pipelineId, $notifications = array()) {
     self::$HttpRequestMethod = 'POST';
     self::$Uri = '/2012-09-25/pipelines/'.$pipelineId.'/notifications';
@@ -256,20 +256,20 @@ class AWS_ET {
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Delete a pipeline
   *
   * @param string $pipelineId pipeline ID
   * @return array | false
-  */   
+  */
   public static function deletePipeline($pipelineId) {
     self::$HttpRequestMethod = 'DELETE';
     self::$Uri = '/2012-09-25/pipelines/'.$pipelineId;
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Test the settings for a pipeline
   *
@@ -278,7 +278,7 @@ class AWS_ET {
   * @param string $role The IAM Amazon Resource Name (ARN) for role to use for transcoding jobs
   * @param array $topics The ARNs of one or more Amazon Simple Notification Service (Amazon SNS) topics
   * @return array | false
-  */   
+  */
   public static function testRole($inputBucket, $outputBucket, $role, $topics = array()) {
     self::$HttpRequestMethod = 'POST';
     self::$Uri = '/2012-09-25/roleTests';
@@ -292,7 +292,7 @@ class AWS_ET {
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Create a new preset
   *
@@ -303,7 +303,7 @@ class AWS_ET {
   * @param array $video video settings
   * @param array $thumbnails thumbnail settings
   * @return array | false
-  */     
+  */
   public static function createPreset($name, $description, $container = 'mp4', $audio = array(), $video = array(), $thumbnails = array()) {
     self::$HttpRequestMethod = 'POST';
     self::$Uri = '/2012-09-25/presets';
@@ -337,7 +337,7 @@ class AWS_ET {
             'PaddingPolicy' => (array_key_exists('PaddingPolicy', $video)) ? $video['PaddingPolicy'] : 'Pad',
             'DisplayAspectRatio' => (array_key_exists('DisplayAspectRatio', $video)) ? $video['DisplayAspectRatio'] : 'auto'
           )
-      );    
+      );
     if (isset($video['Resolution']) && isset($video['AspectRatio'])) {
       unset($requestBody['Video']['MaxWidth']);
       unset($requestBody['Video']['MaxHeight']);
@@ -345,8 +345,8 @@ class AWS_ET {
       unset($requestBody['Video']['PaddingPolicy']);
       unset($requestBody['Video']['DisplayAspectRatio']);
       $requestBody['Video']['Resolution'] = $video['Resolution'];
-      $requestBody['Video']['AspectRatio'] = $video['AspectRatio'];      
-    }    
+      $requestBody['Video']['AspectRatio'] = $video['AspectRatio'];
+    }
     if (isset($video['Watermarks'])) {
         $requestBody['Video']['Watermarks'] = array(
           'Id' => $video['Watermarks']['Id'],
@@ -369,183 +369,183 @@ class AWS_ET {
         'SizingPolicy' => (array_key_exists('SizingPolicy', $thumbnails)) ? $thumbnails['SizingPolicy'] : 'Fit',
         'PaddingPolicy' => (array_key_exists('PaddingPolicy', $thumbnails)) ? $thumbnails['PaddingPolicy'] : 'Pad'
       );
-    // Resolution and AspectRatio aren't recommended per AWS docs      
+    // Resolution and AspectRatio aren't recommended per AWS docs
     if (isset($thumbnails['Resolution']) && isset($thumbnails['AspectRatio'])) {
       unset($requestBody['Thumbnails']['MaxWidth']);
       unset($requestBody['Thumbnails']['MaxHeight']);
       unset($requestBody['Thumbnails']['SizingPolicy']);
       unset($requestBody['Thumbnails']['PaddingPolicy']);
       $requestBody['Thumbnails']['Resolution'] = $thumbnails['Resolution'];
-      $requestBody['Thumbnails']['AspectRatio'] = $thumbnails['AspectRatio'];      
-    }        
+      $requestBody['Thumbnails']['AspectRatio'] = $thumbnails['AspectRatio'];
+    }
     $requestBody = json_encode($requestBody);
     self::setRequestBody($requestBody);
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Get a list of all presets
   *
   * @return array | false
-  */     
+  */
   public static function listPresets() {
     self::$HttpRequestMethod = 'GET';
     self::$Uri = '/2012-09-25/presets';
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Get info about a preset
   *
   * @param string $presetId preset ID
   * @return array | false
-  */    
+  */
   public static function readPreset($presetId) {
     self::$HttpRequestMethod = 'GET';
     self::$Uri = '/2012-09-25/presets/'.$presetId;
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Delete a preset
   *
   * @param string $presetId preset ID
   * @return array | false
-  */     
+  */
   public static function deletePreset($presetId) {
     self::$HttpRequestMethod = 'DELETE';
     self::$Uri = '/2012-09-25/presets/'.$presetId;
     $result = self::sendRequest();
     return $result;
   }
-  
+
   /**
   * Set AWS credentials
   *
   * @param string $awsAccessKey AWS access key
   * @param string $awsPrivateKey AWS private key
   * @return void
-  */     
+  */
   public static function setAuth($awsAccessKey, $awsPrivateKey) {
     self::$AwsAccessKey = $awsAccessKey;
     self::$AwsPrivateKey = $awsPrivateKey;
-  }  
-  
+  }
+
   /**
-  * Get the response HTTP status code 
+  * Get the response HTTP status code
   *
   * @return integer
-  */     
+  */
   public static function getStatusCode() {
     return self::$ResponseStatus;
   }
-  
+
   /**
   * Get server response
   *
   * @return mixed
-  */   
+  */
   public static function getResponse() {
     return self::$Response;
-  }  
-  
+  }
+
   /**
   * Get error message after unsuccessful request
   *
   * @return string
-  */   
+  */
   public static function getErrorMsg() {
     return self::$ErrorMsg;
   }
-  
+
   /**
   * Set request body
   *
   * @param mixed $body request body
   * @return void
-  */  
+  */
   private static function setRequestBody($body) {
     self::$RequestBody = $body;
   }
-  
+
   /**
   * Set request header
   *
   * @param string $key key
-  * @param string $value value  
+  * @param string $value value
   * @return void
-  */   
+  */
   private static function setHeader($key, $value) {
     self::$Headers[$key] = $value;
   }
-  
+
   /**
   * Executes server request
   *
   * @return array | false
-  */  
+  */
   private static function sendRequest() {
-    $endpoint = 'elastictranscoder.'.self::$Region.'.amazonaws.com';  
+    $endpoint = 'elastictranscoder.'.self::$Region.'.amazonaws.com';
     self::$Date = new DateTime('UTC');
     self::setHeader('Host', $endpoint);
     self::setHeader('x-amz-date', self::$Date->format('Ymd\THis\Z'));
     self::setAuthorizationHeader();
-  
+
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, self::$HttpRequestMethod);
     $url = 'https://'.$endpoint . self::$Uri;
-  
+
     if (self::$HttpRequestMethod == 'POST' || self::$HttpRequestMethod == 'PUT') {
       self::setHeader('Content-Type', 'application/json');
       self::setHeader('Content-Length', strlen(self::$RequestBody));
       curl_setopt($curl, CURLOPT_POSTFIELDS, self::$RequestBody);
     }
-  
+
     $headers = array();
     foreach (self::$Headers as $header => $value) {
       if (strlen($value) > 0)
         $headers[] = $header.': '.$value;
     }
-  
+
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($curl, CURLOPT_HEADER, false);
     curl_setopt($curl, CURLOPT_URL, $url);
-  
+
     $result = curl_exec($curl);
-  
+
     if ($result === false)
       self::$ErrorMsg = 'Curl failed. Error code: '.curl_errno($curl).' Message: '.curl_error($curl);
     else
       self::$ResponseStatus  = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-  
+
     @curl_close($curl);
-  
+
     if ($result === false)
       return false;
-      
+
     $response = json_decode($result, true);
     self::$Response = $response;
-    
+
     if (in_array(self::$ResponseStatus, array(200, 201, 202)))
       return $response;
-      
+
     // Apparently "Message" is not always capitalized
     if (isset($response['message']))
-      self::$ErrorMsg = $response['message'];      
+      self::$ErrorMsg = $response['message'];
     if (isset($response['Message']))
       self::$ErrorMsg = $response['Message'];
     return false;
   }
-  
+
   private static function hex16($val) {
     $unpack = unpack('H*', $val);
     return reset($unpack);
   }
-  
+
   private function parseCanonicalUri($url) {
     $parts = parse_url($url);
     $str = '';
@@ -554,41 +554,41 @@ class AWS_ET {
     }
     if (isset($parts['host'])) {
       $str .= $parts['host'];
-    }    
+    }
     if (isset($parts['path'])) {
       $str .= $parts['path'];
-    }    
+    }
     return $str;
   }
-  
+
   private static function parseQueryString($url) {
     $pos = strpos($url, '?');
     if (!$pos)
       return false;
     $url = substr($url, $pos + 1);
     if (empty($url))
-      return false;        
+      return false;
     $pairs = explode('&', $url);
-    $urlVars = array();    
+    $urlVars = array();
     foreach ($pairs as $pair) {
       list($param, $value) = explode('=', $pair, 2);
       $urlVars[$param] = $value;
     }
     return $urlVars;
   }
-    
+
   /**
   * Builds and sets authorization header
   *
   * @return array | false
-  */   
+  */
   private static function setAuthorizationHeader() {
     $canonicalRequest = array();
     $canonicalRequest[] = self::$HttpRequestMethod;
     $canonicalRequest[] = str_replace('%2F', '/', rawurlencode(self::parseCanonicalUri(self::$Uri)));
-    
+
     // Format any query string
-    $queryParams = self::parseQueryString(self::$Uri);    
+    $queryParams = self::parseQueryString(self::$Uri);
     if (!$queryParams) {
       $canonicalRequest[] = '';
     } else {
@@ -600,55 +600,55 @@ class AWS_ET {
       $qs = substr($qs, 0, -1);
       $canonicalRequest[] = $qs;
     }
-  
+
     $headers = array();
     $canonicalHeaders = array();
     $signedHeaders = array();
-  
+
     foreach (self::$Headers as $key => $value) {
       $headers[strtolower($key)] = trim($value);
     }
-  
+
     ksort($headers);
-  
+
     foreach ($headers as $key => $value) {
       $signedHeaders[] = $key;
       $canonicalHeaders[] = $key . ':' . $value;
     }
-  
+
     $signedHeaders = implode(';', $signedHeaders);
-  
+
     $canonicalRequest[] = implode("\n", $canonicalHeaders);
     $canonicalRequest[] = '';
     $canonicalRequest[] = $signedHeaders;
     $canonicalRequest[] = self::hex16(hash('sha256', self::$RequestBody, true));
     $canonicalRequest = implode("\n", $canonicalRequest);
-  
+
     $stringToSign = array();
     $stringToSign[] = 'AWS4-HMAC-SHA256';
     $stringToSign[] = self::$Date->format('Ymd\THis\Z');
-  
+
     $credentialScope = array(self::$Date->format('Ymd'));
     $credentialScope[] = self::$Region;
     $credentialScope[] = 'elastictranscoder';
     $credentialScope[] = 'aws4_request';
     $credentialScope = implode('/', $credentialScope);
-  
+
     $stringToSign[] = $credentialScope;
     $stringToSign[] = self::hex16(hash('sha256', $canonicalRequest, true));
     $stringToSign = implode("\n", $stringToSign);
-  
+
     $kSecret = 'AWS4'.self::$AwsPrivateKey;
     $kDate = hash_hmac('sha256', self::$Date->format('Ymd'), $kSecret, true);
     $kRegion = hash_hmac('sha256', self::$Region, $kDate, true);
     $kService = hash_hmac('sha256', 'elastictranscoder', $kRegion, true);
     $kSigning = hash_hmac('sha256', 'aws4_request', $kService, true);
     $signature = hash_hmac('sha256', $stringToSign, $kSigning, true);
-  
+
     $auth = 'AWS4-HMAC-SHA256 Credential='.self::$AwsAccessKey.'/'.$credentialScope;
     $auth .= ',SignedHeaders='.$signedHeaders;
     $auth .= ',Signature='.self::hex16($signature);
     self::setHeader('Authorization', $auth);
   }
-  
+
 }
